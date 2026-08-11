f6ab712049465d18893232d7ab47db661fdebd1a
sonic7527 <sonic0422@gmail.com>
2026-08-11 20:35:23 +0800

    fix: 在路由層轉址舊文章網址

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      {
+        source:
+          "/posts/2026-07-30-%E7%B6%AD%E4%BF%AE%E8%BB%8A%E6%AC%BEYAMAHA-New-Cuxi-115",
+        destination:
+          "/posts/2026-07-30-yamaha-new-cuxi-115-1053593253836883",
+        permanent: true,
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
